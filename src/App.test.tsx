1a052c52689112f007dfb8ecab4f2d36e719aad6
Frank Stöckli <f@void0.ch>
2021-02-25 21:56:27 +0100

    Add countdown, highscore and name field

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,29 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+test('show the countdown', () => {
+  render(<App />);
+
+  expect(screen.getByText("16:40")).toBeInTheDocument();
+});
+
+test('should show the high score', () => {
+  render(<App />);
+
+  expect(screen.getByText("000000")).toBeInTheDocument();
+});
+
 test('render the input box', () => {
   render(<App />);
-  const inputBox = screen.getByPlaceholderText("Your name");
-  expect(inputBox).toBeInTheDocument();
+
+  expect(screen.getByPlaceholderText("enter your name")).toBeInTheDocument();
+});
+
+test('clear the placeholder on focus', () => {
+  render(<App />);
+  const inputField = screen.getByPlaceholderText("enter your name");
+
+  expect(screen.getByPlaceholderText("enter your name")).toBeInTheDocument();
+  inputField.focus()
+  expect(screen.queryByPlaceholderText("enter your name")).not.toBeInTheDocument();
 });
---
+++ src/components/Timer.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Timer } from './Timer';
+
+test('show the correct time in minutes and seconds', () => {
+  render(<Timer time={1000} />);
+
+  expect(screen.getByText("16:40")).toBeInTheDocument();
+});
+
+test('show leading zeros', () => {
+  render(<Timer time={1} />);
+
+  expect(screen.getByText("00:01")).toBeInTheDocument();
+});
+
+test('count down the seconds', () => {
+  jest.useFakeTimers();
+  render(<Timer time={5} />);
+
+  expect(screen.getByText("00:05")).toBeInTheDocument();
+  jest.advanceTimersByTime(3000)
+  expect(screen.getByText("00:02")).toBeInTheDocument();
+});
+
