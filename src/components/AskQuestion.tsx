21c65ff84eecd33adad6851aaddf7f0f73cf785b
Frank Stöckli <f@void0.ch>
2021-03-06 22:03:50 +0100

    Add retro font to questions

--- src/components/AskQuestion.tsx
+++ src/components/AskQuestion.tsx
@@ -8,8 +8,10 @@ type QuestionProps = {
 export function AskQuestion({ question, updateScore }: QuestionProps) {
   return (
     <>
-      <div className="md:text-6xl sm:text-3xl text-white m-10">
-        <div className="w-screen text-center m-10">{question.question}</div>
+      <div className="md:text-4xl sm:text-3xl text-white m-10 retro-font">
+        <div className="text-center m-5 p-5 text-blue-300">
+          {question.question}
+        </div>
         <div
           className="border-2 p-5 m-5 text-center"
           onClick={() => updateScore()}
