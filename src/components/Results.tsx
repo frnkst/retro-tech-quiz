f39c015cb88817a7ede90832b15021c6723a1724
Frank Stöckli <f@void0.ch>
2021-03-19 18:44:25 +0100

    Ignore package-lock.json and remove recharts

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,13 +1,3 @@
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts'
 import { Result } from './Quiz'
 
 type ResultsProps = {
@@ -19,28 +9,8 @@ export function Results({ results }: ResultsProps) {
     <div className="flex flex-col  w-screen">
       <span className="retro-font md:text-3xl self-center">You suck!</span>
 
-      <div className="bg-gray-800 graph-container self-center">
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={500}
-            height={300}
-            data={results}
-            margin={{
-              top: 20,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="correct" stackId="a" fill="#72d64b" />
-            <Bar dataKey="wrong" stackId="a" fill="#fc5f53" />
-          </BarChart>
-        </ResponsiveContainer>
+      <div className="self-center w-1/4">
+        Results: { JSON.stringify(results) }
       </div>
     </div>
   )
