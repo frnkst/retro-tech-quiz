73d2864cabe101b63ef3ca6b539ba4d80111a83a
Frank Stöckli <f@void0.ch>
2021-03-21 14:44:08 +0100

    Add git questions

--- cypress/integration/screenshots.spec.ts
+++ cypress/integration/screenshots.spec.ts
@@ -1,22 +1,39 @@
 export {}
 describe('Make screenshots for README.md', () => {
-  it('should take a screenshots', () => {
+  beforeEach(() => {
     cy.viewport('macbook-15')
     cy.visit('http://localhost:3000')
+  })
+
+  it('start page', () => {
+    cy.get('input').type('Adam')
+    cy.contains('10:00').click()
+    cy.contains('Web').click()
+    cy.contains('Git').click()
+    cy.contains('TS').click()
 
     cy.screenshot('main_page')
+  })
 
+
+  it('quiz page', () => {
     cy.contains('05:00').click()
     cy.contains('Git').click()
     cy.contains('-->').click()
+    cy.get('button').first().click()
 
     cy.screenshot('quiz_page')
+  })
 
-    cy.visit('http://localhost:3000')
+  it('results page', () => {
     cy.contains('05:00').click()
     cy.contains('Git').click()
     cy.contains('-->').click()
-    cy.contains('-->').click()
+    for (let i=0; i<10; i++) {
+      cy.get('button').first().click()
+      cy.contains('-->').click()
+    }
+
     cy.screenshot('result_page')
   })
 })
--- src/services/question-service.ts
+++ src/services/question-service.ts
@@ -1,4 +1,5 @@
-import { getAllTopics, Option, Topic } from '../components/Categories'
+import { getAllTopics, Topic } from '../components/Categories'
+import { shuffle } from 'lodash'
 
 export function prepareTopics(selectedTopics: string[]) {
   const filteredTopics = getAllTopics().filter((topic) =>
@@ -12,28 +13,7 @@ export function prepareTopics(selectedTopics: string[]) {
 function shuffleOptions(filteredTopics: Topic[]) {
   filteredTopics.forEach((topic) =>
     topic.questions.forEach((question) => {
-      shuffle(question.options)
+      question.options = shuffle(question.options)
     })
   )
-}
-
-// https://github.com/Daplie/knuth-shuffle
-function shuffle(array: Option[]) {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex -= 1
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex]
-    array[currentIndex] = array[randomIndex]
-    array[randomIndex] = temporaryValue
-  }
-
-  return array
 }
