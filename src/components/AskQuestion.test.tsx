f086de04613d91795a8864d72a7418ead63bf69c
Frank Stöckli <f@void0.ch>
2021-03-24 20:41:47 +0100

    Add tests and simplify workflow

--- src/components/AskQuestion.test.tsx
+++ src/components/AskQuestion.test.tsx
@@ -4,30 +4,72 @@ import userEvent from '@testing-library/user-event'
 import { Question } from './Categories'
 
 const selectOption = jest.fn()
-const question = someQuestion()
+let defaultQuestion: Question
 
 beforeEach(() => {
+  defaultQuestion = someQuestion()
+})
+
+test('show the first question and answers', () => {
+  setup()
+
+  expect(screen.getByText(defaultQuestion.question)).toBeVisible()
+  expect(screen.getByText(defaultQuestion.options[0].text)).toBeVisible()
+  expect(screen.getByText(defaultQuestion.options[1].text)).toBeVisible()
+  expect(screen.getByText(defaultQuestion.options[2].text)).toBeVisible()
+})
+
+test('should call selectOption on click', () => {
+  setup()
+  userEvent.click(screen.getByText(defaultQuestion.options[0].text))
+
+  expect(selectOption).toHaveBeenCalled()
+})
+
+describe('show level', () => {
+  test('show the level easy', () => {
+    const easyQuestion: Question = { ...defaultQuestion, level: 2 }
+    setup(easyQuestion)
+
+    expect(screen.getByText('Easy')).toBeVisible()
+  })
+
+  test('show the level medium', () => {
+    const mediumQuestion: Question = { ...defaultQuestion, level: 3 }
+    setup(mediumQuestion)
+
+    expect(screen.getByText('Medium')).toBeVisible()
+  })
+
+  test('show the level hard', () => {
+    const mediumQuestion: Question = { ...defaultQuestion, level: 4 }
+    setup(mediumQuestion)
+
+    expect(screen.getByText('Hard')).toBeVisible()
+  })
+})
+
+test('format the code with prettier', () => {
+  const questionWithCodeSnippet: Question = {
+    ...defaultQuestion,
+    question: 'const a=5; const b=5',
+    codeSnippet: true,
+  }
+  setup(questionWithCodeSnippet)
+
+  expect(screen.getByTestId('code-block')).toBeVisible()
+})
+
+function setup(question: Question = defaultQuestion) {
   render(
     <AskQuestion
+      topicName={'JS'}
       question={question}
       selectOption={selectOption}
       showResult={false}
     />
   )
-})
-
-test('show the first question and answers', () => {
-  expect(screen.getByText(question.question)).toBeVisible()
-  expect(screen.getByText(question.options[0].text)).toBeVisible()
-  expect(screen.getByText(question.options[1].text)).toBeVisible()
-  expect(screen.getByText(question.options[2].text)).toBeVisible()
-})
-
-test('should call selectOption on click', () => {
-  userEvent.click(screen.getByText(question.options[0].text))
-
-  expect(selectOption).toHaveBeenCalled()
-})
+}
 
 function someQuestion(): Question {
   return {
